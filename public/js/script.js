64bd823be19dc3188f029338de25f4cc72308c87
happx <happxjunior@gmail.com>
2025-02-14 19:00:20 -0500

    Enhance matrix cell selection and styling: implement grid layout, add selected state highlighting, and improve event handling for cell clicks.

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,7 @@
+
+selectedCell = null;
+cells = null;
+
 async function request(endpoint, method, body) {
     const response = await fetch(`http://localhost:3000/api/${endpoint}`, {
         method,
@@ -20,10 +24,11 @@ function renderMatrix() {
         const container = document.getElementById('matrix');
         container.innerHTML = '';
         matrix.forEach((row, x) => {
-            const rowDiv = document.createElement('div');
             row.forEach((value, y) => {
                 const cell = document.createElement('div');
                 cell.className = 'cell';
+                cell.dataset.x = x;
+                cell.dataset.y = y;
                 if (value === 0) {
                     cell.style.backgroundColor = `rgba(0, 0, 0, 0.4)`;
                 } else {
@@ -31,9 +36,26 @@ function renderMatrix() {
                     const green = Math.round(255 * (1 - value));
                     cell.style.backgroundColor = `rgb(${red}, ${green}, 0)`;
                 }
-                rowDiv.appendChild(cell);
+                if (selectedCell?.dataset.x == x && selectedCell?.dataset.y == y) {
+                    cell.classList.add('selected');
+                    selectedCell = cell;
+                }
+                container.appendChild(cell);
             });
-            container.appendChild(rowDiv);
+        });
+
+        cells = Array.from(document.getElementsByClassName('cell'));
+        cells.forEach(cell => {
+            cell.addEventListener('click', () => {
+                if (cell.classList.contains('selected')) {
+                    selectedCell?.classList.remove('selected');
+                    selectedCell = null;
+                    return;
+                }
+                selectedCell?.classList.remove('selected');
+                selectedCell = cell
+                cell.classList.add('selected');
+            });
         });
     });
 }
@@ -51,5 +73,9 @@ async function adjustAll(value) {
 function handleSliderChange(event) {
     const value = parseFloat(event.target.value);
     document.getElementById('slider-value').innerText = value.toFixed(2);
-    adjustAll(value);
+    if (selectedCell) {
+        return updateValue(selectedCell.dataset.x, selectedCell.dataset.y, value);
+    }else {
+        adjustAll(value);
+    }
 }
